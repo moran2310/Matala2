b0cc22c03eafc6be6c2c0a193d961c21c7c2bad6
moran2310 <moranrohana5@gmail.com>
2025-01-19 21:37:46 +0200

    Add files via upload

---
+++ node-ex6/node-ex6/app.js
@@ -0,0 +1,21 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const dirPath = path.join(__dirname, "/templates");
+const htmlFilePath = path.join(dirPath, "page.html");
+const cssFilePath = path.join(dirPath, "style.css");
+
+const server = http.createServer((req, res) => {
+  if (req.url === "/style.css") {
+    res.setHeader("Content-Type", "text/css");
+    fs.createReadStream(cssFilePath).pipe(res);
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    fs.createReadStream(htmlFilePath).pipe(res);
+  }
+});
+
+server.listen(3000, () => {
+  console.log("Server working on port 3000");
+});
